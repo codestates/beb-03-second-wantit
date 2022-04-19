ce820f252da5230b45ab53b3d5709afdb01f6d5e
journiyoon <journiyoon@gmail.com>
2022-04-19 17:54:18 +0900

    initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const cors = require("cors");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+	cors({
+		origin: ["http://localhost:3000/"],
+		credentials: true,
+		methods: ["GET", "POST", "OPTIONS", "PATCH"],
+	})
+);
+
+app.get("/", (req, res) => {
+	res.send("wantit Backend Server");
+});
+
+server = app.listen(4000);
+console.log("http server runnning!!");
