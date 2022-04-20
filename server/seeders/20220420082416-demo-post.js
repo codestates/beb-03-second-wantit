febead780365472b1151c211cea46e9a1c5107c3
espressom <espressom@naver.com>
2022-04-20 17:35:29 +0900

    [1] 더미데이터 생성

---
+++ server/seeders/20220420082416-demo-post.js
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+    id
+title
+body
+createdAt
+user_id
+    */
+    return queryInterface.bulkInsert("Posts", [
+      {
+        title: "첫 번째 글",
+        body: "첫 번째 내용",
+        user_id: 1,
+      },
+      {
+        title: "두 번째 글",
+        body: "두 번째 내용",
+        user_id: 1,
+      },
+      {
+        title: "세 번째 글",
+        body: "세 번째 내용",
+        user_id: 1,
+      },
+      {
+        title: "네 번째 글",
+        body: "네 번째 내용",
+        user_id: 1,
+      },
+      {
+        title: "다섯 번째 글",
+        body: "다섯 번째 내용",
+        user_id: 1,
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Posts", null, {});
+  },
+};
