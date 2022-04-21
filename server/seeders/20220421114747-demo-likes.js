acaf9f57be3012620ff046f15fd9c5462c861299
journiyoon <journiyoon@gmail.com>
2022-04-21 23:05:34 +0900

    Add like seeder

--- server/seeders/20220421114747-demo-likes.js
+++ server/seeders/20220421114747-demo-likes.js
@@ -4,27 +4,22 @@ module.exports = {
 	up: (queryInterface, Sequelize) => {
 		return queryInterface.bulkInsert("Likes", [
 			{
-				content: "첫 번째 좋아요입니다",
 				user_id: 1,
 				post_id: 1,
 			},
 			{
-				content: "두 번째 좋아요입니다",
 				user_id: 1,
 				post_id: 1,
 			},
 			{
-				content: "세 번째 좋아요입니다",
 				user_id: 1,
 				post_id: 2,
 			},
 			{
-				content: "네 번째 좋아요입니다",
 				user_id: 1,
 				post_id: 2,
 			},
 			{
-				content: "다섯 번째 좋아요입니다",
 				user_id: 1,
 				post_id: 3,
 			},
