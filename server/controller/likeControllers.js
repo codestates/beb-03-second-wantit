99ddde93c640a956ef9be60b0dd0bee142c2989c
journiyoon <journiyoon@gmail.com>
2022-04-22 03:00:12 +0900

    Add function like, deleteLike

--- server/controller/likeControllers.js
+++ server/controller/likeControllers.js
@@ -3,42 +3,51 @@ const { Posts, Likes } = require("../models");
 module.exports = {
 	// 좋아요 추가 핸들러
 	like: async (req, res) => {
-		// console.log(req.body);
-		const id = req.params.id;
+		const { user_id, post_id } = req.body;
 		try {
-			/* const liked = await Likes.create({
-				user_id,
-				post_id,
-			}); */
-			const post = await Posts.findOne({ where: { id } });
-			const like = await Likes.create({
-				post,
+			const liked = await Likes.findOrCreate({
+				where: {
+					user_id: user_id,
+					post_id: post_id,
+				},
+				defaults: {
+					user_id: user_id,
+					post_id: post_id,
+				},
 			});
-			// const liked = await post.addLike();
-			if (like) {
-				res.status(201).send({ message: "Click like button" });
+			if (liked) {
+				res.status(204).send({ message: "Click like button" });
 			} else {
-				res.status(400).send({ message: "Not click" });
+				res.status(404).send({ message: "Not click" });
 			}
 		} catch (e) {
 			res.status(500).send({ message: "Error click like button" });
-			// console.error(e);
+			console.error(e);
 		}
 	},
 	// 좋아요 취소 핸들러
 	deleteLike: async (req, res) => {
 		const id = req.params.id;
+		const deleted = await Likes.destroy({
+			include: [
+				{
+					model: Posts,
+					attributes: ["id"],
+					where: {
+						id: id,
+					},
+				},
+			],
+			where: { id },
+		});
 		try {
-			const deleted = await Likes.destroy({
-				where: { id },
-			});
 			if (deleted) {
-				res.status(200).send({ message: "Successfully canceled like button" });
+				res.status(204).send({ message: "Successfully canceled like" });
 			} else {
-				res.status(404).send({ message: "Not canceled like button" });
+				res.status(404).send({ message: "Not canceled like" });
 			}
 		} catch (e) {
-			res.status(500).send("Failed to canceled like button");
+			res.status(500).send("Failed to canceled like");
 			console.error(e);
 		}
 	},
