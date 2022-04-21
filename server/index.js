21833e47f38ec623e642c0b19ae2e9942f29afe7
espressom <espressom@naver.com>
2022-04-21 18:12:47 +0900

    [2] dotenv 및 config.js 작업

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const usersRouter = require("./router/usersRouter.js");
 const postRouter = require("./router/postRouter.js");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,3 +24,4 @@ app.use("/post", postRouter);
 
 server = app.listen(4000);
 console.log("http server runnning!!");
+console.log(process.env.DATABASE_PASSWORD);
