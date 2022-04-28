902e111c033d190876e0d47efd667e781b794aeb
espressom <espressom@naver.com>
2022-04-28 19:33:42 +0900

    redux-persist 적용 완료

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,19 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
-import rootReducer from "./modules";
+import Loading from "./pages/Loading";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import configureStore from "./modules/configureStore.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const store = createStore(rootReducer);
+
+const { store, persistor } = configureStore();
 
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    <PersistGate loading={<Loading />} persistor={persistor}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </PersistGate>
   </Provider>
 );
